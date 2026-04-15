98a23e383f5302df74e458ac2c4a92195018edf9
aris8 <aris8@D4LTAFALAH>
2026-04-15 11:47:41 +0700

    Add project files.

---
+++ ifelse/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ifelse.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("role") != "admin" )
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            ViewBag.Username = HttpContext.Session.GetString("username");
+            return View();
+        }
+    }
+}
---
+++ ifelse/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ifelse.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ifelse.Controllers
+{
+    public class LoginController : Controller
+    {
+        public static List<User> users = new List<User>()
+        {
+            new User { Username = "admin_ceo", Password = "123" }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username, string password)
+        {
+            if (username == "admin_ceo" && password == "123")
+            {
+                HttpContext.Session.SetString("username", username);
+                HttpContext.Session.SetString("role", "admin");
+                return RedirectToAction("Index", "Admin");
+            }
+
+            ViewBag.Error = "Username atau password salah";
+            return View();
+
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string username, string password)
+        {
+            if (users.Any(u => u.Username == username))
+            {
+                ViewBag.Error = "username sudah dipakai";
+                return View();
+            }
+
+            users.Add(new User
+            {
+                Username = username,
+                Password = password
+            });
+
+            return RedirectToAction("index");
+        }
+    }
+}
+
